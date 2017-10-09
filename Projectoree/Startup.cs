87a6d2a43c1b9ce7fdab2385de1508400c5b638d
David112 <david.winslade@live.com>
2017-10-09 22:38:14 +0930

    ASP.NET Projectoree Core
    Core generated files for Projectoree

---
+++ Projectoree/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Projectoree.Startup))]
+namespace Projectoree
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
